fa568ad030d6a6a31b4274ef9994ac1f8bff5292
Petar <marincicpetar@gmail.com>
2025-04-17 21:57:26 +0200

    add Cusotmer implements observer

--- src/App/Models/Customer.php
+++ src/App/Models/Customer.php
@@ -5,17 +5,20 @@ namespace App\Models;
 use App\Core\PaymentValidator;
 use App\Core\Validator;
 use App\Interfaces\CustomerInterface;
+use App\Interfaces\ObserverInterface;
 use Exception;
 
-class Customer implements CustomerInterface
+class Customer implements CustomerInterface, ObserverInterface
 {
    private string $name = "";
    private float $saldo = 0.00;
    private ?int $cardNumber;
+   private ?string $email;
 
 
 
-   public function __construct(string $name, float $saldo, ?int $cardNumber = null)
+
+   public function __construct(string $name, float $saldo, ?string $email = null,  ?int $cardNumber = null)
    {
 
       $this->setName($name);
@@ -23,6 +26,9 @@ class Customer implements CustomerInterface
 
       if ($cardNumber) {
          $this->setCard($cardNumber);
+      }
+      if ($email) {
+         $this->setEmail($email);
       }
    }
 
@@ -60,12 +66,24 @@ class Customer implements CustomerInterface
       }
       return $this->cardNumber  = $cardNumber;
    }
+   public function setEmail(string $email): string
+   {
 
+      if (! Validator::email($email)) {
+         throw new Exception("Invalid email");
+      }
+
+      return $this->email  = $email;
+   }
 
 
    public function getCard(): int|null
    {
       return $this->cardNumber;
+   }
+   public function getEmail(): string|null
+   {
+      return $this->email;
    }
 
 
@@ -77,5 +95,10 @@ class Customer implements CustomerInterface
    public function getName(): string
    {
       return $this->name;
+   }
+
+   public function update($eventData): void
+   {
+      new SendMail($this, $eventData);
    }
 }
